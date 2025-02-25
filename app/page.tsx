45a1d17eaa298bdca34303b38693481e05df97b1
kinich barcelo <kinich@kbdreams.com>
2025-02-25 11:23:52 +0100

    add api routes for login register logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div className="flex flex-wrap gap-4">
-      <p className="text-red-500"> yolo</p>
+      <p className="text-red-500" onClick={() => mutate()}>
+        {" "}
+        yolo
+      </p>
     </div>
   );
 }
