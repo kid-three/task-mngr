167a0ecfe27d7bebf9d5e171d107b669edfcc7e7
kinich barcelo <kinich@kbdreams.com>
2025-03-12 11:07:45 +0100

    fix schema

--- features/workspaces/schemas.ts
+++ features/workspaces/schemas.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 export const createWorkspaceSchema = z.object({
   name: z.string().trim().min(1, "Required"),
-  image: z.any().optional(),
-  // .union([
-  //   z.instanceof(File),
-  //   z.string().transform((value) => (value === "" ? undefined : value)),
-  // ])
-  // .optional(),
+  image: z
+    .union([
+      z.instanceof(Blob),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
 });
